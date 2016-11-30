95186d880630d86a41a1dffa72761a8d32cd8c1b
Rahel Lüthy <rahel.luethy@gmail.com>
2016-11-30 17:54:19 +0100

    #4: Add BimgurFX client application

---
+++ bimgur-activiti-fx-client/src/main/java/ch/fhnw/ima/bimgur/client/BimgurFX.java
@@ -0,0 +1,43 @@
+package ch.fhnw.ima.bimgur.client;
+
+import javafx.application.Application;
+import javafx.application.Platform;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public final class BimgurFX extends Application {
+
+    private static final Logger LOG = Logger.getLogger(BimgurFX.class.getSimpleName());
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        StackPane rootPane = new StackPane(new Label("Hello Bimgur"));
+        Scene scene = new Scene(rootPane, 800, 600);
+
+        stage.setOnCloseRequest(e -> {
+            LOG.fine("Main window closed, exiting application");
+            Platform.exit();
+            System.exit(0);
+        });
+
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        // Exit VM if main frame is closed
+        Platform.setImplicitExit(true);
+        try {
+            launch(args);
+        } catch (Throwable t) {
+            LOG.log(Level.SEVERE, t.getMessage(), t);
+            System.exit(-1);
+        }
+    }
+
+}
