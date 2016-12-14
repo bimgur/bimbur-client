01a5170a845051a0cddce9e1db3c2084075574a3
Rahel Lüthy <rahel.luethy@gmail.com>
2016-12-14 15:31:41 +0100

    #5: Use a process engine to facilitate setup/teardown of tests

--- bimgur-activiti-rest-client/src/test/java/ch/fhnw/ima/bimgur/activiti/TestUtils.java
+++ bimgur-activiti-rest-client/src/test/java/ch/fhnw/ima/bimgur/activiti/TestUtils.java
@@ -1,13 +1,25 @@
 package ch.fhnw.ima.bimgur.activiti;
 
+import org.activiti.engine.ProcessEngine;
+import org.activiti.engine.ProcessEngineConfiguration;
+
 public interface TestUtils {
 
-    String BASE_URL = "http://192.168.99.100:8080/activiti-rest/service/";
+    String HOST = "localhost"; // TODO: make configurable on OSX
+    String BASE_URL = "http://" + HOST + ":8080/activiti-rest/service/";
     String USER_NAME = "kermit";
     String PASSWORD = "kermit";
+    String DB_URL = "jdbc:postgresql://" + HOST + ":5432/bimgurdb";
 
     static ActivitiRestClient client() {
         return ActivitiRestClient.connect(BASE_URL, USER_NAME, PASSWORD);
+    }
+
+    static ProcessEngine processEngine() {
+        return ProcessEngineConfiguration
+                .createProcessEngineConfigurationFromResourceDefault()
+                .setJdbcUrl(DB_URL)
+                .buildProcessEngine();
     }
 
 }
